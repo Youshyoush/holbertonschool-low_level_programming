5ce371ce46c24420cfe05006c2abb1d8aa2a2d5d
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-02-20 17:41:58 +0100

    documentation

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-    char letter;
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-    putchar('\n');
+	{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
 
-    return (0);
-}
+	return (0);
+	}

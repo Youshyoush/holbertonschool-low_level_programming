3f9755faf056788934459282f9baedaf5bde9e20
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-02-20 17:57:57 +0100

    betty coding style verification

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-	{
+{
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-	putchar(letter);
+		putchar(letter);
 	}
 	putchar('\n');
 
 	return (0);
-	}
+}

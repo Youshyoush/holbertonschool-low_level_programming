fd8c8dfda058713a85f17e4b4e1abe258f0af567
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-02-20 17:26:18 +0100

    delete trailing space

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** 
- *Last digit print the last digit of a random number and return a text 
+/**
+ *Last digit print the last digit of a random number and return a text
  *when that last digit is greater than 5 or less than 6 and not 0.
- *Always return (0) 
+ *Always return (0)
  */
 int main(void)
 {

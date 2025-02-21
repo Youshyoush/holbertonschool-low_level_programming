4e8c8c237583ac43777851d49beaac791d799845
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-02-21 18:07:36 +0100

    updating main.h with c style commentage

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);          // Declare _putchar function
-void print_alphabet(void);     // Declare print_alphabet function
+int _putchar(char c);          /*  Declare _putchar function */
+void print_alphabet(void);     /*  Declare print_alphabet function */
 
 #endif

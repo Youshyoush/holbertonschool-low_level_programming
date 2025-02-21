4a4123ccf7efa742ec55a9565faa4cb2f1688530
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-02-21 17:30:27 +0100

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _putchar('_');  /* Print the underscore character */
+    _putchar('p');  /* Print the character 'p' */
+    _putchar('u');  /* Print the character 'u' */
+    _putchar('t');  /* Print the character 't' */
+    _putchar('c');  /* Print the character 'c' */
+    _putchar('h');  /* Print the character 'h' */
+    _putchar('a');  /* Print the character 'a' */
+    _putchar('r');  /* Print the character 'r' */
+    _putchar('\n'); /* Print a new line */
+
+    return (0);     /* Return 0 to indicate success */
+}

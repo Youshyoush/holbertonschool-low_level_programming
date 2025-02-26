c411e7a814539f05e9ced18e4956ba28fd43f77a
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-02-26 16:52:58 +0100

    betty coding style & betty documentation

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,15 @@
 /**
  * int _strlen - returns the length of a string
  * @s: pointer string.
- *
+ * 
+ * Return: length of the string.
  */
 
 int _strlen(char *s)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	while (*s != '\0')
 	{
 		i++;

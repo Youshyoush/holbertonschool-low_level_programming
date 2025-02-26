9d024e7f89f4e64a1ba1bf8f0bfd9934e8ba99ae
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-02-26 16:55:58 +0100

    delete trailing space & correction on the main function description

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * int _strlen - returns the length of a string
+ * _strlen - returns the length of a string
  * @s: pointer string.
- * 
+ *
  * Return: length of the string.
  */
 

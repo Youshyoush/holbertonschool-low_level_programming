9bfac3b38df6f77ffc56404ec9b8bbdb477ad40a
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-03-01 16:37:50 +0100

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every 2 character of a string.
+ * @str: string pointer.
+ *
+ */
+
+void puts2(char *str)
+{
+	int len = 0, j;
+
+	while (len != '\0')
+	{
+		len++
+	}
+	for (j = 0; j % 2 = 0; j++)
+	{
+		if (j == 0)
+		{
+			_putchar(*str);
+		}
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}

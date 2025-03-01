7176c27850d624453f33cd7811a690fcd2febb27
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-03-01 17:51:44 +0100

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,17 +9,15 @@
 
 int _atoi(char *s)
 {
-	int sign = 1; // Sign of the number (1 for positive, -1 for negative)
-	int result = 0; // Stores the converted integer
+	int sign = 1;
+	int result = 0;
 	int i = 0;
 
-	/* Skip leading spaces */
 	while (s[i] == ' ')
 	{
 		i++;
 	}
 
-	/* Handle optional sign */
 	if (s[i] == '-' || s[i] == '+')
 	{
 		if (s[i] == '-')
@@ -32,7 +30,7 @@ int _atoi(char *s)
 	{
 		i++;
 	}
-	/* Convert digits to integer */
+
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		result = result * 10 + (s[i] - '0');

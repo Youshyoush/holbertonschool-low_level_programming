097ab264116c981e6df5ba5b7dc04e7b4ad4c28c
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-03-01 17:43:50 +0100

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy a string from src point to to pointer dest with the null termitator
+ * @dest: destination pointer where the string is pointed to
+ * @src: the pointer string to be copied
+ *
+ * Return : dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}

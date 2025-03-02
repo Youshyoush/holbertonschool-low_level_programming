42f1415fca28221e83038149e3d0f55f2ed8f30f
Youshyoush”git config --global user.name Youshyoush” <xiao.yuchen@yahoo.com>
2025-03-02 19:11:10 +0100

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer
@@ -6,10 +7,11 @@
  *
  * Return: The converted integer, or 0 if no valid numbers are found
  */
+
 int _atoi(char *s)
 {
 	int sign = 1;
-	int num = 0;
+	unsigned int num = 0;
 	int found_digit = 0;
 
 	while (*s)
@@ -18,8 +20,13 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 		{
-			num = num * 10 + (*s - '0');
 			found_digit = 1;
+			if (num > (INT_MAX / 10) || (num == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
+			{
+				return (sign == 1 ? INT_MAX : INT_MIN);
+			}
+
+			num = num * 10 + (*s - '0');
 		}
 		else if (found_digit)
 			break;
